3b9e95868a526edff8b3838ececcee5afe7d08f2
rashiop <red.alpacca@gmail.com>
2021-05-10 22:28:47 +0700

    solution: add webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import './style/style.css';
+
+import $ from 'jquery';
+import moment from 'moment';
+
+
 const displayTime = () => {
   moment.locale( "id" );
   $( ".time" ).text( moment().format( "LTS" ) );
