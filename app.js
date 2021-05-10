03d89bcaf88fa863c2c737522069a553c5eff7e1
rashiop <red.alpacca@gmail.com>
2021-05-10 19:02:49 +0700

    solution: web component

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+import './src/script/component/app-bar.js';
+import './src/script/component/club-list.js';
+import './src/script/component/search-bar.js';
+
 import main from './src/script/view/main.js';
 
-document.addEventListener("DOMContentLoaded", main);
+
+document.addEventListener( "DOMContentLoaded", main );
--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,40 +1,31 @@
+import '../component/club-list.js';
+import '../component/search-bar.js';
+
 import DataSource from '../data/data-source.js';
 
 const main = () => {
-    const searchElement = document.querySelector("#searchElement");
-    const buttonSearchElement = document.querySelector("#searchButtonElement");
-    const clubListElement = document.querySelector("#clubList");
+    const searchElement = document.querySelector( "search-bar" );
+    const clubListElement = document.querySelector( "club-list" );
 
     const onButtonSearchClicked = async () => {
         try {
-            const filteredClubs = await DataSource.searchClub(searchElement.value);
-            renderResult(filteredClubs)
-        } catch(ex) {
-            fallbackResult(ex);
+            const filteredClubs = await DataSource.searchClub( searchElement.value );
+            console.log( 'value', filteredClubs )
+            renderResult( filteredClubs )
+        } catch ( ex ) {
+            fallbackResult( ex );
         }
     };
 
-    const renderResult = (results) => {
-        clubListElement.innerHTML = "";
-        results.forEach(({name, fanArt, description}) => {
-            const clubElement = document.createElement("div");
-            clubElement.setAttribute("class", "club");
-
-            clubElement.innerHTML = '<img class="fan-art-club" src="' + fanArt + '" alt="Fan Art">\n' +
-                '<div class="club-info">\n' +
-                '<h2>' + name + '</h2>\n' +
-                '<p>' + description + '</p>' +
-                '</div>';
-            clubListElement.appendChild(clubElement);
-        })
+    const fallbackResult = ( message ) => {
+        clubListElement.renderError( message )
     };
 
-    const fallbackResult = function (message) {
-        clubListElement.innerHTML = "";
-        clubListElement.innerHTML += '<h2 class="placeholder">' + message + '</h2>'
+    const renderResult = results => {
+        clubListElement.clubs = results;
     };
 
-    buttonSearchElement.addEventListener("click", onButtonSearchClicked);
+    searchElement.clickEvent = onButtonSearchClicked;
 };
 
 export default main;
